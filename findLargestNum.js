0c1e48ee2d60a241c311d7ec9afeefadc435f8b1
AhmedSami019 <easami019192@gmail.com>
2025-10-13 20:16:57 +0600

    find largest number form an array is compleate

--- findLargestNum.js
+++ findLargestNum.js
@@ -1,3 +1,10 @@
 const numbers = [32, 21, 54, 32, 63, 74, 59]
+let largestNum = numbers[0]
 
-numbers
+numbers.map(num => {
+    // console.log(num);
+    if(num > largestNum){
+        largestNum = num
+    }
+})
+console.log(largestNum, "is the largest number");
