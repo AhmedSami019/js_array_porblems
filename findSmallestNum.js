d6ca0163c2de43bb68d0c612004c2d1a24dcf95d
AhmedSami019 <easami019192@gmail.com>
2025-10-13 20:18:56 +0600

    find smallest number form an arry is compleated

---
+++ findSmallestNum.js
@@ -0,0 +1,12 @@
+const numbers = [32, 21, 54, 32, 63, 74, 59]
+let largestNum = numbers[0]
+
+numbers.map(num => {
+    // console.log(num);
+    if(num < largestNum){
+        largestNum = num
+    }
+})
+console.log(largestNum, "is the smallest number");
+
+// findSmallestNum.js
