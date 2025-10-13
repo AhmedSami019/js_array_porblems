fac7be182b1515d2eb9ef360c5521608e738bc66
AhmedSami019 <easami019192@gmail.com>
2025-10-13 20:12:37 +0600

    sum of all number in array is compleated

---
+++ findLargestNum.js
@@ -0,0 +1,3 @@
+const numbers = [32, 21, 54, 32, 63, 74, 59]
+
+numbers
--- getSumOfAllNum.js
+++ getSumOfAllNum.js
@@ -1,7 +1,7 @@
 const numbers = [33, 53, 66, 63, 12, 6, 52]
 let sumOfAllNum = 0
 
-const sumAl = numbers.map(num =>{
+numbers.map(num =>{
     sumOfAllNum = num + sumOfAllNum
 })
 console.log(sumOfAllNum);
